03ce6c4021a0751f9872ab9cc6ed5f83a7f8cf6d
laiyao1 <laiyao1@163.com>
2017-09-13 16:15:18 +0800

    use mult-thread&blocking function -> now it will not lose data

--- chatting_room/chatting_room/server.cpp
+++ chatting_room/chatting_room/server.cpp
@@ -1,111 +1,126 @@
+//有些情况下末位字符丢失，需修改读入的次数，改为多次连续读入，直到收不到字符
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+
 #include<iostream>
 #include<cstdio>
 #include<string>
-#include <boost/asio.hpp>
-#include <boost/array.hpp>
+
 #include <thread>
 using namespace std;
 using namespace boost::asio;
 
-unsigned short port_number;
-string user_name;
-
-string send_message;
-bool message_flag;
-int len;
-void input_data()
+class session
 {
-	while (1)
-	{
-		cout << "input data..." << endl;
-		//getline(cin, send_message);
-		cin >> send_message;
-		send_message = user_name + ":" + send_message;
-		message_flag = true;
-		while (message_flag == true);
-		send_message = "";
-	}
-}
-void handler1(const boost::system::error_code& error, // Result of operation.
-	std::size_t bytes_transferred           // Number of bytes read.
-)
-{
-	size_t len = bytes_transferred;
-}
-
-void set_up(unsigned short port_number)
-{
+public:
+	string user_name;
+	string send_message;
+	unsigned short port_number;
+	size_t len;
 	io_service ios;
 	ip::address addr;
-	addr = addr.from_string("127.0.0.1");
-	cout << "server starting..." << endl;
+	ip::tcp::socket *sock_ptr;
 
-	ip::tcp::acceptor acceptor(ios, ip::tcp::endpoint(addr, port_number));
-	cout << acceptor.local_endpoint().address() << ":" << acceptor.local_endpoint().port() << endl;
-
-	ip::tcp::socket sock(ios);//建立socket
-	acceptor.accept(sock);//accept
-
-	cout << "connected to client successfully." << endl;
-	std::cout << "client: " << sock.remote_endpoint().address() << ":" << sock.remote_endpoint().port() << std::endl;
-
-	vector<char> recv_message(128);
-
-	//同步读写
-	/*
-	while (1)
+	session(string user_name, unsigned short port_number):
+		user_name(user_name), port_number(port_number)
 	{
-		getline(cin, send_message);
-		//cin >> send_message;
-		send_message = user_name + ":" + send_message;
-		sock.write_some(boost::asio::buffer(send_message));
-		size_t len = sock.read_some(boost::asio::buffer(recv_message));
-		std::cout.write(recv_message.data(), len);
-		cout << endl;
-	}*/
+		string server_ip;
+		//cout << "please input the server ip:" << endl;
+		//cin >> server_ip;
 
-	//异步读写
-	thread t_input( input_data );
-	t_input.detach();
-	while (1)
+		addr = addr.from_string("127.0.0.1");
+		cout << "server starting..." << endl;
+
+		ip::tcp::acceptor acceptor(ios, ip::tcp::endpoint(addr, port_number));
+		cout << acceptor.local_endpoint().address() << ":" << acceptor.local_endpoint().port() << endl;
+
+		sock_ptr = new ip::tcp::socket(ios);//建立socket//建立socket
+		acceptor.accept(*sock_ptr);//accept
+
+		cout << "connected to client successfully." << endl;
+		std::cout << "client: " << sock_ptr->remote_endpoint().address() << ":" << sock_ptr->remote_endpoint().port() << std::endl;
+	}
+
+	static void input_data(class session* ptr)
 	{
-		if (message_flag == true)
+		while (1)
 		{
-			sock.write_some(boost::asio::buffer(send_message));
-			message_flag = false;
+			//cout << "input data..." << endl;
+			cin >> ptr->send_message;
+			ptr->send_message = ptr->user_name + ":" + ptr->send_message;
+			ptr->sock_ptr->write_some(boost::asio::buffer(ptr->send_message));
 		}
-		/*size_t len = sock.read_some(boost::asio::buffer(recv_message));
-		if (len > 0)
+	}
+
+	void set_up()
+	{
+		vector<char> recv_message(128);
+		//同步读写
+		/*
+		while (1)
 		{
+			getline(cin, send_message);
+			//cin >> send_message;
+			send_message = user_name + ":" + send_message;
+			sock.write_some(boost::asio::buffer(send_message));
+			size_t len = sock.read_some(boost::asio::buffer(recv_message));
 			std::cout.write(recv_message.data(), len);
 			cout << endl;
 		}*/
+		//异步读写
+		thread t_input(input_data, this);
+		t_input.detach();
 
-		for (auto i = recv_message.begin(); i != recv_message.end(); i++)
-			*i = 0;
-		sock.async_read_some(boost::asio::buffer(recv_message,128), handler1);
-		if (recv_message[0])
+		while (1)
 		{
-			for (auto i = recv_message.begin(); i != recv_message.end() && *i; i++)
-				cout << *i;
-			//cout << recv_message.data();
-			cout << endl;
+			size_t len = sock_ptr->read_some(boost::asio::buffer(recv_message));
+			if (len > 0)
+			{
+				std::cout.write(recv_message.data(), len);
+				cout << endl;
+			}
 		}
+		/*
+		while (1)
+		{
+			if (message_flag == true)
+			{
+				sock.write_some(boost::asio::buffer(send_message));
+				message_flag = false;
+			}
+
+			for (auto i = recv_message.begin(); i != recv_message.end(); i++)
+				*i = 0;
+
+			boost::asio::async_read(sock, boost::asio::buffer(recv_message, 128), handler);
+			if (recv_message[0])
+			{
+				for (auto i = recv_message.begin(); i != recv_message.end() && *i; i++)
+					cout << *i;
+				cout << endl;
+			}
+		}*/
 	}
-}
+};
 int main()
 {
 	cout<<"welcome to chatting room!"<<endl;
 	
-	cout<<"please input your username:"<<endl;
-	cin >> user_name;
-	
+	//cout<<"please input your username:"<<endl;
+	//cin >> user_name;
+	string user_name;
+	user_name = "laiyao1";
+	cout << "user name has been set to laiyao1" << endl;
+	unsigned short port_number;
+	port_number = 9876;
 	while (port_number < 1024)
 	{
-		cout<<"please input the port number"<<endl;
-		scanf_s("%hd", &port_number);
+		//cout<<"please input the port number"<<endl;
+		//scanf_s("%hd", &port_number);
+		port_number = 9876;
+		cout << "port number has been set to 9876" << endl;
 	}
-	set_up(port_number);
-
+	session se(user_name, port_number);
+	se.set_up();
 	return 0;
 }
--- chatting_room_client/chatting_room_client/client.cpp
+++ chatting_room_client/chatting_room_client/client.cpp
@@ -1,3 +1,5 @@
+//有些情况下末位字符丢失，需修改读入的次数，改为多次连续读入，直到收不到字符
+
 #include<iostream>
 #include<cstdio>
 #include<string>
@@ -8,106 +10,122 @@
 using namespace std;
 using namespace boost::asio;
 
-string user_name;
-string send_message;
-unsigned int port_number;
-bool message_flag;
-size_t len;
+class session {
 
-void handler1(const boost::system::error_code& error, // Result of operation.
-	std::size_t bytes_transferred           // Number of bytes read.
-)
-{
-	len = bytes_transferred;
-}
-
-void input_data()
-{
-	while (1)
-	{
-		cout << "input data..." << endl;
-		//getline(cin, send_message);
-		//cin.getline(send_message);
-		cin >> send_message;
-		send_message = user_name + ":" + send_message;
-		message_flag = true;
-		while (message_flag == true);
-		send_message = "";
-	}
-}
-
-void set_up(unsigned int port_number)
-{
+public:
+	string user_name;
+	string send_message;
+	unsigned int port_number;
+	size_t len;
 	io_service ios;
 	ip::address addr;
-	string server_ip;
-	cout << "please input the server ip:" << endl;
-	cin >> server_ip;
-	addr = addr.from_string(server_ip);
+	ip::tcp::socket *sock_ptr;
 
-	cout << "connecting to server..." << endl;
-	ip::tcp::endpoint ep(addr, port_number);
-
-	ip::tcp::socket sock(ios);//建立socket
-
-	sock.connect(ep);
-	std::cout << "server: " << sock.remote_endpoint().address() << ":" << sock.remote_endpoint().port() << std::endl;
-
-	vector<char> recv_message(128);
-	/*
-	while (1)
+	session(string user_name,unsigned short port_number):
+		user_name(user_name),port_number(port_number)
 	{
-		//第一个版本(同步通信)
-		//该版本程序必须server & client双方发完信息才能互相看到收到的信息
-		getline(cin, send_message);
-		send_message = user_name + ":" + send_message;
-		sock.write_some( boost::asio::buffer(send_message) );
-		size_t len = sock.read_some( boost::asio::buffer(recv_message) );
-		std::cout.write(recv_message.data(), len);
-		cout << endl;
-	}*/
-	//异步读写
-	thread t_input(input_data);
-	t_input.detach();
-	while (1)
+		string server_ip;
+		//cout << "please input the server ip:" << endl;
+		//cin >> server_ip;
+
+		server_ip = "127.0.0.1";
+		cout << "server ip has been set to 127.0.0.1" << endl;
+
+		addr = addr.from_string(server_ip);
+
+		cout << "connecting to server..." << endl;
+		ip::tcp::endpoint ep(addr, port_number);
+
+		sock_ptr = new ip::tcp::socket(ios);//建立socket
+
+		sock_ptr->connect(ep);
+		std::cout << "server: " << (sock_ptr->remote_endpoint()).address() << ":" << (sock_ptr->remote_endpoint()).port() << std::endl;
+	}
+
+	static void input_data(class session* ptr)
 	{
-		if (message_flag == true)
+		while (1)
 		{
-			sock.write_some(boost::asio::buffer(send_message));
-			message_flag = false;
+			//cout << "input data..." << endl;
+			cin >> ptr->send_message;
+			ptr->send_message = ptr->user_name + ":" + ptr->send_message;
+			ptr->sock_ptr->write_some(boost::asio::buffer(ptr->send_message));
 		}
-		/*size_t len = sock.read_some(boost::asio::buffer(recv_message));
-		if (len > 0)
+	}
+
+	void set_up()
+	{
+		vector<char> recv_message(128);
+		/*
+		while (1)
 		{
+			//第一个版本(同步通信)
+			//该版本程序必须server & client双方发完信息才能互相看到收到的信息
+			getline(cin, send_message);
+			send_message = user_name + ":" + send_message;
+			sock.write_some( boost::asio::buffer(send_message) );
+			size_t len = sock.read_some( boost::asio::buffer(recv_message) );
 			std::cout.write(recv_message.data(), len);
 			cout << endl;
 		}*/
-		for (auto i = recv_message.begin(); i != recv_message.end(); i++)
-			*i = 0;
-		sock.async_read_some(boost::asio::buffer(recv_message, 128), handler1);
-		if (recv_message[0])
+		//异步读写
+		thread t_input(input_data, this);
+		t_input.detach();
+
+		while (1)
 		{
-			for (auto i = recv_message.begin(); i != recv_message.end() && *i; i++)
-				cout << *i;
-			//cout << recv_message.data();
-			cout << endl;
+			size_t len = sock_ptr->read_some(boost::asio::buffer(recv_message));
+			if (len > 0)
+			{
+				std::cout.write(recv_message.data(), len);
+				cout << endl;
+			}
 		}
+		/*
+		while (1)
+		{
+			if (message_flag == true)
+			{
+
+				cout<<sock.write_some(boost::asio::buffer(send_message))<<endl;
+				message_flag = false;
+			}
+			size_t len = sock.read_some(boost::asio::buffer(recv_message));
+			if (len > 0)
+			{
+				std::cout.write(recv_message.data(), len);
+				cout << endl;
+			}
+			for (auto i = recv_message.begin(); i != recv_message.end(); i++)
+				*i = 0;
+			boost::asio::async_read(sock, boost::asio::buffer(recv_message, 10), handler);
+			if (recv_message[0])
+			{
+				for (auto i = recv_message.begin(); i != recv_message.end() && *i; i++)
+						cout << *i;
+				cout << endl;
+			}
+		}*/
 	}
-}
+};
 
 int main()
 {
 	cout << "welcome to chatting room! You are a client port." << endl;
-
 	cout << "please input your username:" << endl;
-	cin >> user_name;
-
+	//cin >> user_name;
+	string user_name;
+	user_name = "laiyao2";
+	cout << "user name has been set to laiyao2" << endl;
+	unsigned short port_number;
+	port_number = 9876;
 	while (port_number < 1024)
 	{
-		cout << "please input the port number:(the number must be the same as the server port number)" << endl;
-		cin >> port_number;
-
+		//cout << "please input the port number:(the number must be the same as the server port number)" << endl;
+		//cin >> port_number;
+		port_number = 9876;
+		cout << "port number has been set to 9876" << endl;
 	}
-	set_up(port_number);
-
+	session se(user_name,port_number);
+	se.set_up();
 }
